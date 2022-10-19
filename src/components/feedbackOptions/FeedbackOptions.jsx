286cd82ad93da0845274513ebb51cb832683c9f6
Ulyanna <ulyanna16@gmail.com>
2022-10-19 22:43:26 +0300

    add styled button

--- src/components/feedbackOptions/FeedbackOptions.jsx
+++ src/components/feedbackOptions/FeedbackOptions.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import css from './FeedbackOptions.module.css';
+import { Button } from './FeedbackButton.styled';
 export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   return (
     <ul className={css.FeedbackOptionsList}>
       {options.map(option => {
         return (
           <li key={option}>
-            <button type="button" onClick={() => onLeaveFeedback(option)}>
+            <Button
+              type="button"
+              optionType={option}
+              onClick={() => onLeaveFeedback(option)}
+            >
               {option}
-            </button>
+            </Button>
           </li>
         );
       })}
